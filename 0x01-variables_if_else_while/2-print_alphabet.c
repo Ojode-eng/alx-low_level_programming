ff316b3c149cde3bd67062bb0ccaaf1e4b463f1e
Ojode-eng <odhiambofelix996@gmail.com>
2022-07-04 17:19:08 -0700

    3-Print

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * return: always return 0
+ */
+
+int main (void)
+
+{
+	char a;
+
+	for (a = "a"; a <= "z"; a++)
+		
+		putchar(a);
+
+	purchar ('\n');
+
+	return (0);
+
+}
